2d32a5dfcad4eb7ea542a343ff29ce58d1748fef
Michael De Wree <michael.dewree@ordina.be>
2018-12-11 13:53:27 +0100

    Built-in validation

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
 import { Customer } from './customer';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +8,38 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.customerForm = this.builder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Mich',
+      lastName: 'De Wree',
+      email: 'mich@ordina.be',
+      sendCatalog: false
+    });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators([Validators.required]);
+    } else {
+      phoneControl.clearAsyncValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
